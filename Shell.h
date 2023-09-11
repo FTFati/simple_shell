3511cb5fe21d23b0af179cdf161ce6c257e91f73
ZZZHajar008 <ilyass.infinite345@gmail.com>
2023-09-11 10:22:03 -0700

    C - Simple Shell

--- Shell.h
+++ Shell.h
@@ -9,5 +9,7 @@
 
 char *_strcpy(char *dest, char *src);
 void execute(char **av);
+int execute_command(char **agv, char *buf);
+void tokenizes(char *buff, char **tok);
 
 #endif
---
+++ getline.c
@@ -0,0 +1,36 @@
+#include "Shell.h"
+
+#define READ_SIZE 1024
+
+/**
+ * _getline - Gets a line from a file descriptor.
+ * @ln: Pointer to store the line.
+ * @bufsize: Size of the buffer.
+ * @fd: File descriptor.
+ *
+ * Return: Number of characters read, -1 on failure.
+ */
+ssize_t _getline(char **ln, size_t *bufsize, int fd)
+{
+	ssize_t n_chars = 0;
+
+	if (ln == NULL || bufsize == NULL)
+		return (-1);
+
+	if (*ln == NULL)
+	{
+		*ln = malloc(sizeof(char) * READ_SIZE);
+		if (*ln == NULL)
+			return (-1);
+		*bufsize = READ_SIZE;
+	}
+
+	n_chars = read(fd, *ln, *bufsize);
+	if (n_chars == -1)
+	{
+		free(*ln);
+		return (-1);
+	}
+
+	return (n_chars);
+}
